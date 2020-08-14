3821f2114bd746e13cff39401eecb5c99f0e0420
ezau <ezaumateus97@gmail.com>
2020-08-13 22:27:16 -0300

    criandoVarRefereteAsOpcoes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert('helloword')
+var nome = document.querySelector("#exampleInputName");
+
+var gender = document.querySelectorAll("form-user-create [name=gender]:checked")
+
+var birth = document.querySelector("#exampleInputBirth")
+var country = document.querySelector("#exampleInputCountry")
+var email = document.querySelector("#exampleInputEmail")
+var pass = document.querySelector("#exampleInputPassword")
+var photo = document.querySelector("#exampleInputFile")
+var adm = document.querySelector("#exampleInputAdmin")
