5f4bad11f270adfef0ef467ca4b18fb52813faa8
ezau <ezaumateus97@gmail.com>
2020-08-13 23:30:08 -0300

    checkedGender

--- index.js
+++ index.js
@@ -15,10 +15,17 @@ var fields = document.querySelectorAll("#form-user-create [name]")
 fields.forEach(function(field,index){
 
     if(field.name == "gender") {
+        
+        if(field.checked){
+        console.log("sim", field);
+        }
+        
+    }else{
 
+        console.log("não")
     }
 
-    console.log(field.checked)
+    //console.log(field.id, field.name, field.nodeValue, index)
 
 
 
