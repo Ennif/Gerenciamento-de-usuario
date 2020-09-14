d3081c6d8028d0d7656c354428a53dba99400794
ezau <ezaumateus97@gmail.com>
2020-09-14 19:49:33 -0300

    addStaticParaPegarHora

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,10 +37,7 @@ class UserController {
        
         })
 
-        
-        
-        
-        
+              
     ;
 }
 
@@ -115,7 +112,7 @@ class UserController {
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${(dataUser.admin)?"sim" : "nao"}</td>
-            <td>${dataUser.register}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
